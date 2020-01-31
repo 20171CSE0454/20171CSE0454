9eda4bda8a27afaf043b8d8d1c2855a4cda2b650
20171CSE0454 <60249651+20171CSE0454@users.noreply.github.com>
2020-01-31 09:40:58 +0530

    Add files via upload

---
+++ html.c
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head><title>list</title></head>
+<body background-color="lightblue">
+<ul>
+<li>item 1</li>
+<li>item 2</li>
+</ul>
+<ol type="I" start="3">
+<li>item 1</li>
+<li>item 2</li>
+</ol>
+<dl>
+<dt>HTML</dt>
+<dd>Hypertext Markup Language</dd>
+<dt>Dog</dt>
+<dd>A human's best friend</dd>
+</body>
+</html>
+
